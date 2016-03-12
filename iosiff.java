f2abd1d7147bb0c99b75ed7ea8e88d328e356e3c
roshkov <nikitar98@inbox.lv>
2016-03-12 22:33:10 +0200

    Added files via upload

---
+++ iosiff.java
@@ -0,0 +1,66 @@
+/**
+ * Created by java on 3/6/2016.
+ */
+public class iosiff {
+    public static void main(String[] args) {
+
+        int n = Integer.parseInt(args[0]);
+        int m = Integer.parseInt(args[1]) - 1;  //kazhdij tretij == +2
+
+        int j;
+        int i;
+        int q = 0;   //q=[0..4]
+        int l = 0; //last
+        int lc=0;//last kolvo;
+
+        int[] mass = new int[n];
+        for (i = 0; i <= n - 1; i++) {
+            mass[i] = 1;
+        }
+        System.out.print("hello");
+
+        while (lc !=1) {
+
+            lc = 0;
+            /////
+            for (i = 1; i <= m; i++) {            //perehod na m shagov
+
+                while (mass[q] == 0) {
+                    q = q + 1;
+                    if (q > n - 1) {
+                        q = q - n;
+                    }
+                }
+                q = q + 1;
+                if (q > n - 1) {
+                    q = q - n;
+                }
+
+                while (mass[q] == 0) {
+                    q = q + 1;
+                    if (q > n - 1) {
+                        q = q - n;
+                    }
+                }
+
+            }
+
+            mass[q] = 0;
+            q = q + 1;
+
+
+            ////
+
+            for (i = 0; i <= n - 1; i++) {
+                if (mass[i] == 1) {
+                    lc = lc + 1; //kolvo
+                    l=q; //poslednij nomer gde stoit "1"
+                }
+            }
+
+        }
+        System.out.println("Lucky #" + (l + 1) + " has survived");
+
+    }
+}
+
