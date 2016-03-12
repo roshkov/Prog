f5ac26b84aeb5943e4b595213a0f972e1216a7da
roshkov <nikitar98@inbox.lv>
2016-03-13 00:15:30 +0200

    Initial commit

---
+++ figuri/src/figuri.java
@@ -0,0 +1,21 @@
+/**
+ * Created by java on 1/31/2016.
+ */
+public class figuri {
+    public static void main(String[] args) {
+        abstract void paint();
+
+    }
+
+   public class rectangle extends figuri{
+       void paint();
+
+   }
+
+  public class
+}
+
+
+
+
+
